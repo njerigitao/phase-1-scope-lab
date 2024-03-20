66fe1c2e84c1780d591c45648d1710645fc8bb60
AGNESGITAU <agnesgitau40@gmail.com>
2024-03-20 15:35:24 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customername = 'bob';
+
+function upperCaseCustomerName(){
+   return upperCaseCustomerName('BOB');
+
+function bestcustomer(){
+return(bestcustomer('not bob'));
+};
+function overwritesBestCustomer(){
+   return (overwritesBestCustomer('maybeBob'));
+};
+const leastFavoriteCustomer = 'Simon';
+
+function changeLeastFavoriteCustomer(){
+   return (leastFavoriteCustomer('Elkan'));
+}};
